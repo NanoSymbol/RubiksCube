3a80e3e19b7f40682cb459c077b6aedc27a91725
NanoSymbol <130672939+NanoSymbol@users.noreply.github.com>
2023-04-13 18:29:55 +0200

    Add files via upload

---
+++ Primjer.py
@@ -0,0 +1,12 @@
+from Cube import Kocka
+kocka = Kocka()
+
+kocka.printaj_kocku()
+print(kocka.rjesenakocka())
+kocka.unos_poteza("UURRLLDDBBFF")
+kocka.printaj_kocku()
+print(kocka.rjesenakocka())
+kocka.printaj_kocku
+kocka.scramble()
+kocka.printaj_kocku()
+print("Izmjesana kocka")
