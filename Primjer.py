ae0e9ec2fdbc2d79968e58d7b6d514b025cc89db
NanoSymbol <130672939+NanoSymbol@users.noreply.github.com>
2023-05-25 16:21:31 +0200

    Update Primjer.py

--- Primjer.py
+++ Primjer.py
@@ -1,17 +1,12 @@
+import matplotlib.pyplot as plt
 from Cube import Kocka
-from evolucijski_algoritam import evolutionary_algorithm
+from GrafickiPrikaz import PrikazKocke
 
 kocka = Kocka()
-
-kocka.printaj_kocku()
-print(kocka.rjesenakocka())
-kocka.unos_poteza("UURRLLDDBBFF")
-kocka.printaj_kocku()
-print(kocka.rjesenakocka())
-kocka.printaj_kocku
 kocka.scramble()
+kocka.unos_poteza("F D D R L U F F R F U L D F U F L D L F F D L L F L D D U D R L F R F F L D F U D U R D R F U D F D D R U D D F F L R U D R F R F U R D F U L D U U D U D L R L U D ")
+kocka_prikaz = PrikazKocke(kocka)
+kocka_prikaz.prikazi_kocku()
+plt.show()
 kocka.printaj_kocku()
-print("Izmjesana kocka")
 
-#Primjer za evolucijski algoritam
-evolutionary_algorithm()
